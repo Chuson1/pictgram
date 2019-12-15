1ee84afabca02aaf1030ab6e33fa265346224cce
Koki Nakamura <a77k21mn@yahoo.co.jp>
2019-12-15 12:01:19 +0000

    Add to new.html.erb + user.rb validates

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+	validates :name, presence: true, length: { maximum: 15 }
+	
+	VALID_EMAIL_REGEX =  /\A\S+@\S+\.\S+\z/
+	validates :email, 
+		presence: true,
+		format: { with: VALID_EMAIL_REGEX }
+	
+	VALID_PASSWORD_REGEX = /^(?=.*?[a-z])(?=.*?\d)[a-z\d]$/i
+	validates :password, 
+		length: { minimum: 8, maximum: 32},
+		format: { with: VALID_PASSWORD_REGEX }
+		
+	validates :password_confirmation, 
+		length: { minimum: 8, maximum: 32},
+		format: { with: VALID_PASSWORD_REGEX }
+		
+	has_secure_password
+end
